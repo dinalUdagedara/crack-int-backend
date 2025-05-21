d24491036788ea6ae399824e2703298636abd15f
Dinal Udagedara <126741968+dinalUdagedara@users.noreply.github.com>
2025-05-21 22:59:58 +0530

    feat: script added and new route added

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-# backend/main.py
-
+from typing import Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Allow requests from your Next.js frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # change if using production frontend
+    # change if using production frontend
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @app.get("/api/hello")
 def hello():
     return {"message": "Hello from FastAPI!"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
