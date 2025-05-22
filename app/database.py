ca47f46a4fa5c9c64bd2cb05d6a607fbd865314c
Dinal Udagedara <126741968+dinalUdagedara@users.noreply.github.com>
2025-05-22 18:05:06 +0530

    fix: folder structure refactored

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
+from typing import Annotated
+from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
 
 
 URL_DATABASE = 'postgresql://postgres:12345678@localhost:5432/crack-int'
@@ -8,6 +11,19 @@ URL_DATABASE = 'postgresql://postgres:12345678@localhost:5432/crack-int'
 
 engine = create_engine(URL_DATABASE)
 
-SessionLocal = sessionmaker(autocommit=False , autoflush= False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
 
 Base = declarative_base()
---
+++ app/schemas.py
@@ -0,0 +1,12 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class ChoiceBase(BaseModel):
+    choice_text: str
+    is_correct: bool
+
+
+class QuestionBase(BaseModel):
+    question_text: str
+    choices: List[ChoiceBase]
